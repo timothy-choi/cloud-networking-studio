4624079306cf7238d5f7af284c192f9e5603fa5d
Timothy Choi <tchoi720@gmail.com>
2026-06-01 18:26:52 -0700

    fixing a db migration test failures

--- backend/alembic/versions/20260606_topology_placement.py
+++ backend/alembic/versions/20260606_topology_placement.py
@@ -7,7 +7,7 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "20260606_topology_placement_plans"
+revision: str = "20260606_topology_placement"
 down_revision: Union[str, None] = "20260605_gcp_project_id"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
